c39b97bbc6c7d60675249a83b339524957956010
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2021-02-12 06:12:30 -0500

    lab2 update feb12

--- labs/lab2/js/app.js
+++ labs/lab2/js/app.js
@@ -6,23 +6,24 @@ var radius = 20;
 //setting the size of the canvas:
 
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(400, 400);
+
+
+    function draw() {
+        background(176, 171, 171);
+        ellipse(mouseX, mouseY, radius, radius);
+        fill(3, 115, 252);
+    }
+
+    // couldn't figure out how to make it change colors based on position
+
+    function draw() {
+        background(176, 171, 171);
+        ellipse(mouseX, mouseY, radius, radius);
+        fill(252, 3, 3);
+    }
+
 }
-
-function draw() {
-  background(176, 171, 171);
-  ellipse(mouseX, mouseY, radius, radius);
-  fill(3, 115, 252);
-}
-
-// couldn't figure out how to make it change colors based on position
-
-function draw() {
-  background(176, 171, 171);
-  ellipse(mouseX, mouseY, radius, radius);
-  fill(252, 3, 3);
-}
-
 //WORLD WRAP******************************************************
 
 var x = 0;
@@ -32,29 +33,30 @@ var xspeed = 5;
 var yspeed = 0;
 
 function setup() {
-  createCanvas(800, 600);
+    createCanvas(800, 600);
+
+
+    function draw() {
+        background(166, 225, 255);
+
+        x = x + xspeed;
+        y = y + yspeed;
+
+        fill(177, 46, 242);
+
+        if (x > 800) {
+            x = 0;
+
+            console.log("greater than");
+        }
+
+        circle(x, y, 30);
+    }
+
 }
-
-function draw() {
-  background(166, 225, 255);
-
-  x = x + xspeed;
-  y = y + yspeed;
-
-  fill(177, 46, 242);
-
-  if (x > 800) {
-    x = 0;
-
-    console.log("greater than");
-  }
-
-  circle(x, y, 30);
-}
-
 // COUNTER *********************************************************
 let counter = 0;
 function draw() {
-  counter = counter + 1;
-  console.log(counter);
+    counter = counter + 1;
+    console.log(counter);
 }
