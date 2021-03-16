dbbe05f1e9640be5cae5a607fa6b99cf8985222e
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2021-03-16 15:34:53 -0400

    lab5 update
    updated parts of the code

--- labs/lab4/nepolitan/js/app.js
+++ labs/lab4/nepolitan/js/app.js
@@ -4,11 +4,12 @@ let arrayColors = ["#0099ff", "#e600ff", "#00ff51"];
 
 function setup() {
     createCanvas(500, 400);
-    background(145, 242, 255);
+    background(145, 232, 200);
 }
 function draw() {
     for (var count = 0; count < arrayColors.length; count++) {
         fill(arrayColors[count]);
         rect(80 + 100 * count, 80, 80);
+
     }
 }
--- labs/lab5/stoplight/js/app.js
+++ labs/lab5/stoplight/js/app.js
@@ -4,12 +4,12 @@
 let stoplight = ["#F62727", "#E9ED30", "#00ff51"];
 
 function setup() {
-    createCanvas(350, 150);
+    createCanvas(600, 300);
     background(27, 28, 2);
 }
 function draw() {
     for (var count = 0; count < stoplight.length; count++) {
         fill(stoplight[count]);
-        circle(80 + 100 * count, 80, 80);
+        circle(100 + 190 * count, 140, 80);
     }
 }
