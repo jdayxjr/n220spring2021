522c597eba6a99f80942670a8512e569b6135fc6
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2021-03-16 21:42:26 -0400

    update app js
    app js

--- labs/lab4/nepolitan/js/app.js
+++ labs/lab4/nepolitan/js/app.js
@@ -4,12 +4,12 @@ let arrayColors = ["#0099ff", "#e600ff", "#00ff51"];
 
 function setup() {
     createCanvas(500, 400);
-    background(145, 232, 200);
+    background(145, 242, 255);
 }
+
 function draw() {
     for (var count = 0; count < arrayColors.length; count++) {
         fill(arrayColors[count]);
         rect(80 + 100 * count, 80, 80);
-
     }
 }
