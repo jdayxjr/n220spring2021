22f7321dff5bc7c3f793a6d2a22532fa6549e450
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2021-05-04 22:12:01 -0400

    lab4 changed

--- labs/lab4/nepolitan/js/app.js
+++ labs/lab4/nepolitan/js/app.js
@@ -1,15 +1,16 @@
-//Neapolitan**********************************************************
 
-let arrayColors = ["#0099ff", "#e600ff", "#00ff51"];
+
+let colors = ["#fffcf8", "#ffc0cb", "#7e3f12"]
+
 
 function setup() {
-    createCanvas(500, 400);
-    background(145, 242, 255);
-}
 
-function draw() {
-    for (var count = 0; count < arrayColors.length; count++) {
-        fill(arrayColors[count]);
-        rect(80 + 100 * count, 80, 80);
+    createCanvas(400, 300)
+    background(193, 202, 214)
+    for (var count = 0; count < colors.length; count++) {
+        fill(colors[count]);
+        rect(50 * count, 50, 60, 50);
+
     }
 }
+
